dc7db07577f908623e89f9a6bed8b88189852075
Oscar Juárez <juarezgonz02@gmail.com>
2021-06-24 23:16:30 -0600

    add Apointments View Model

---
+++ Programacion Orientada a Objetos/Codigo fuente/Controls/AppointmentsVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace VaccinationManagement.Controls
+{
+    public class AppointmentsVm 
+    {
+        public int Codigo_Cita { get; set; }
+        
+        public string DUI { get; set; }
+        public string Ciudadano { get; set; }
+        public DateTime Fecha_Cita { get; set; }
+        public string Tipo_Cita { get; set; }
+
+        public AppointmentsVm()
+        {
+            
+        }
+    }
+}
--- Programacion Orientada a Objetos/Codigo fuente/Program.cs
+++ Programacion Orientada a Objetos/Codigo fuente/Program.cs
@@ -7,6 +7,7 @@ using VaccinationManagement.View;
 
 namespace VaccinationManagement
 {
+    
     static class LocationData
     {
         public static int UserId { get; set; }
