c97a2b992aaaa0881c93a869a53b0bc42a535fd5
Robertocruzz <00210619@gmail.com>
2021-06-26 19:25:52 -0600

    verificationappointment

---
+++ Programacion Orientada a Objetos/Codigo fuente/Models/connections.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+
+#nullable disable
+namespace VaccinationManagement.Models
+{
+    public class connections
+    {
+        private SqlConnection conn = new SqlConnection();
+        SqlCommand cod;
+        
+        
+
+    }
+}
---
+++ Programacion Orientada a Objetos/Codigo fuente/Views/Frmverification.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace VaccinationManagement.Views
+{
+    public partial class Frmverification : Form
+    {
+        public Frmverification()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            borrarmensajeerror();
+            if (validarcampos())
+            {
+                MessageBox.Show("datos bien ingresados");
+            }
+                
+        }
+
+        private bool validarcampos()
+        {
+            bool ok = true;
+
+            if (textBox1 == "")
+            {
+                ok = false;
+                ErrorProvider.SetError(textBox1, "ingresar su nombre")
+            }
+
+            return ok;
+        }
+
+        private void borrarmensajeerror()
+        {
+            ErrorProvider.SetError(textBox1, "")
+        }
+    }
+}
